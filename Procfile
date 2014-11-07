7b44ed0a7a4b5a210a6908358368dae08104e5a0
Henri Tremblay <henri@tremblay.pro>
2014-11-07 16:40:02 -0500

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/kingpin-standalone.jar clojure.main -m kingpin.web
---
+++ test/kingpin/web_test.clj
@@ -0,0 +1,6 @@
+(ns kingpin.web-test
+  (:require [clojure.test :refer :all]
+            [kingpin.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
