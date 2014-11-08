1b8a5f1affa21dc197d188de370bc4fff57b8f47
Henri Tremblay <henri@tremblay.pro>
2014-11-07 20:13:39 -0500

    Add DB

--- project.clj
+++ project.clj
@@ -10,7 +10,9 @@
                  [ring-basic-authentication "1.0.5"]
                  [environ "0.5.0"]
                  [com.cemerick/drawbridge "0.0.6"]
-                 [camel-snake-kebab "0.2.4"]]
+                 [camel-snake-kebab "0.2.4"]
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [postgresql "9.1-901-1.jdbc4"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
